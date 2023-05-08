a8c0cccf1be785e812729869140e93965eff2088
Hong-ming <hongming.xiang01@estudiant.upf.edu>
2023-05-08 17:08:59 +0200

    Particles + Alien movement

--- Assets/Scripts/AlienWalk.cs
+++ Assets/Scripts/AlienWalk.cs
@@ -6,6 +6,12 @@ public class AlienWalk : MonoBehaviour
 {
     public int speed;
     private bool isMovingLeft = false;
+    private bool rotating = false;
+
+    private float rotationTime = 2f;
+    private float rotationSpeed = 3f;
+
+    private Quaternion newRotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +21,13 @@ public class AlienWalk : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!rotating)
+        {
+            rotating = true;
+            StartCoroutine(RotateAlien());
+            rotating = false;
+        }
+
         if (isMovingLeft)
         {
             transform.position = new Vector3(transform.position.x,
@@ -23,7 +36,9 @@ public class AlienWalk : MonoBehaviour
             if (transform.position.z <= 5)
             {
                 isMovingLeft = false;
+                newRotation = Quaternion.Euler(320f, 0f, 0f);
             }
+
         }
         else
         {
@@ -33,7 +48,16 @@ public class AlienWalk : MonoBehaviour
             if (transform.position.z >= 95)
             {
                 isMovingLeft = true;
+                newRotation = Quaternion.Euler(320f, 180f, 0f);
             }
+            
         }
+    }
+
+    private IEnumerator RotateAlien()
+    {
+        float t = Mathf.Clamp(Time.deltaTime * rotationSpeed, 0f, 0.99f);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, t);
+        yield return new WaitForSeconds(rotationTime);
     }
 }
