51af6eda929fac5cfdc426b2c5136160effc2922
homexiang3 <65497259+homexiang3@users.noreply.github.com>
2023-05-20 14:22:58 +0200

    Update 1.1
    Ship template change materials
    Change player representation

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -6,9 +6,11 @@ public class PlayerCollision : MonoBehaviour
 {
     public AudioSource audioPlayer;
 
-    [SerializeField]
-    private float forceMagnitude;
     private GameObject ObjectHolded;
+    public Material Material;
+    public Material Material001;
+    public Material Material002;
+    public Material Material003;
 
     // Start is called before the first frame update
     void Start()
@@ -27,12 +29,10 @@ public class PlayerCollision : MonoBehaviour
 
     public void OnTriggerEnter(Collider collision)
     {
-        Debug.Log("isCollision");
-        Debug.Log(collision.tag);
 
-        if ((collision.tag == "Alien" || collision.tag == "Tree") && !audioPlayer.isPlaying)
+        if (collision.tag == "Alien"  && !audioPlayer.isPlaying)
         {
-            Debug.Log("Collision");
+            Debug.Log("Alien is Happy");
             audioPlayer.Play();
         }
         if (collision.tag == "Planet")
@@ -40,7 +40,7 @@ public class PlayerCollision : MonoBehaviour
             Debug.Log("Planet Collision");
         }
 
-        if (collision.tag == "ShipBody" || collision.tag == "ShipArms" || collision.tag == "ShipMotor" || collision.tag == "ShipEye") // Level1: Cuando player colisiona con ship
+        if (collision.tag == "ShipBody" || collision.tag == "ShipArms" || collision.tag == "ShipMotor" || collision.tag == "ShipEye") // Level1: Cuando player colisiona con piezas del ship
         {
             Debug.Log(collision.tag);
             ObjectHolded = collision.gameObject;
@@ -54,10 +54,40 @@ public class PlayerCollision : MonoBehaviour
                 for (int i = 0; i < collision.gameObject.transform.childCount; i++) // Loop through each childObject
                 {
                     GameObject child = collision.gameObject.transform.GetChild(i).gameObject;
-                    if (ObjectHolded.CompareTag(child.tag)) // If same tag
+                    if (ObjectHolded.CompareTag(child.tag)) // Find a match 
                     {
-                        Debug.Log("Alpha");
-                        break;
+
+                        switch (child.tag)
+                        {
+                            case "ShipBody": //change body material
+                                GameObject body = child.gameObject.transform.GetChild(0).gameObject;
+                                GameObject belt = child.gameObject.transform.GetChild(1).gameObject;
+                                GameObject head = child.gameObject.transform.GetChild(2).gameObject;
+                                body.GetComponent<Renderer>().material = Material;
+                                belt.GetComponent<Renderer>().material = Material001;
+                                head.GetComponent<Renderer>().material = Material002;
+                                break;
+                            case "ShipArms":
+                                GameObject arm = child.gameObject.transform.GetChild(0).gameObject;
+                                arm.GetComponent<Renderer>().material = Material002; //Change arm material
+                                break;
+                            case "ShipMotor":
+                                GameObject motor = child.gameObject.transform.GetChild(0).gameObject;
+                                motor.GetComponent<Renderer>().material = Material001; //Change ship material
+                                break;
+                            case "ShipEye": //change eye material
+                                GameObject eye1 = child.gameObject.transform.GetChild(0).gameObject;
+                                GameObject eye2 = child.gameObject.transform.GetChild(1).gameObject;
+                                GameObject eye3 = child.gameObject.transform.GetChild(2).gameObject;
+                                eye1.GetComponent<Renderer>().material = Material001;
+                                eye2.GetComponent<Renderer>().material = Material003;
+                                eye3.GetComponent<Renderer>().material = Material002;
+                                break;
+                            default:
+                                Debug.Log("Incorrect ship tag");
+                                break;
+                        }
+                        break; //stop iterating over other ship childs
                     }
                 }
 
