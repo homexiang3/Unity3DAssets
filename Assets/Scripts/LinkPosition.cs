0368f2017d0bfbbe58027d6c8b037c21a34956d5
homexiang3 <hongming.xiang01@estudiant.upf.edu>
2023-06-02 12:17:01 +0200

    Level3 link

--- Assets/Scripts/LinkPosition.cs
+++ Assets/Scripts/LinkPosition.cs
@@ -5,24 +5,71 @@ using UnityEngine;
 public class LinkPosition : MonoBehaviour
 
 {
-    public Transform Player1Pos;
-    public Transform Player2Pos;
-    private Vector3 center;
-    private Vector3 rotation;
-    private Vector3 size;
     // Start is called before the first frame update
-    //TO DO: Rotate and rescale on player movement
+    public GameObject go1;
+    public GameObject go2;
+    LineRenderer l;
+    private Color near = Color.green;
+    private Color far = Color.red;
+    private float distance;
+    private GameObject link;
+
     void Start()
     {
-        Vector3 center = ((Player1Pos.position + Player2Pos.position) * 0.5f);
-        transform.position = center;
-
+        l = gameObject.AddComponent<LineRenderer>();
+        l.material = new Material(Shader.Find("Legacy Shaders/Particles/Alpha Blended Premultiply"));
+        distance = 0.0f;
+        link = new GameObject("link", typeof(CapsuleCollider));
+        CapsuleCollider capsuleCollider = link.GetComponent<CapsuleCollider>();
+        capsuleCollider.direction = 0;
+        capsuleCollider.center = new Vector3(0, 0, 0);
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 center = ((Player1Pos.position + Player2Pos.position) * 0.5f);
-        transform.position = center;
+        distance = Vector3.Distance(go1.transform.position, go2.transform.position);
+        if(distance < 20.0f)
+        {
+            l.SetColors(near,near);
+
+        }
+        else
+        {
+            l.SetColors(far,far);
+
+        }
+
+        List<Vector3> pos = new List<Vector3>();
+        pos.Add(go1.transform.position);
+        pos.Add(go2.transform.position);
+        l.startWidth = 0.5f;
+        l.endWidth = 0.5f;
+        l.SetPositions(pos.ToArray());
+        l.useWorldSpace = true;
+        ColliderUpdate();
+    }
+
+    void ColliderUpdate()
+    {
+        Vector3 startPos = l.GetPosition(0);
+        Vector3 endPos = l.GetPosition(1);
+
+        Vector3 dir = endPos - startPos;  ///---this is vector that points from starting point to finish point
+        float height = dir.magnitude;
+
+        Vector3 dir2 = dir * 0.5f;
+        Vector3 centerCoord = startPos + dir2;
+
+        float angleWithZ = Vector3.Angle(dir, new Vector3(0, 0, 1));
+        float angleWithY = Vector3.Angle(dir, new Vector3(0, 1, 0));
+        float angleWithX = Vector3.Angle(dir, new Vector3(1, 0, 0));
+
+        link.transform.position = centerCoord;
+        CapsuleCollider capsuleCollider = link.GetComponent<CapsuleCollider>();
+        capsuleCollider.height = height;
+        capsuleCollider.transform.rotation = Quaternion.Euler(angleWithX, angleWithY, angleWithZ);
     }
 }
